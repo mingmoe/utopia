f1eaaa2d10ac8318049e065f677cfb9574496d3c
mingmoe <sudo.free@qq.com>
2022-07-27 15:10:48 +0800

    feat(core): move source_location impl from standard to source

--- utopia-core/include/utopia/abort.hpp
+++ utopia-core/include/utopia/abort.hpp
@@ -13,7 +13,6 @@
 
 #include <boost/stacktrace.hpp>
 #include <exception>
-#include <source_location>
 #include <sstream>
 #include <string_view>
 
@@ -22,6 +21,7 @@
 #endif
 
 #include "utopia/iostream.hpp"
+#include "utopia/source_location.hpp"
 
 namespace utopia::core {
 
@@ -50,15 +50,15 @@ namespace utopia::core {
      * @param location 由编译器自动填写。调用位置。
      * @note 断言函数在运行时也将会存在。要消除开销使用u_debug_assert宏
     */
-    inline constexpr void u_assert(
-        bool                   condition,
-        const std::string_view reason,
-        std::source_location   location = std::source_location::current()) {
+    inline constexpr void
+        u_assert(bool                   condition,
+                 const std::string_view reason,
+                 source_location        location = source_location::current()) {
 
         if(!condition) {
             fmt::print(
                 stdout,
-                "====> abort <====\nassert failed down:{}\nat {}:{}:{} {}()",
+                "====> abort <====\nassert failed down:{}\nat {}({}:{}) {}()",
                 reason,
                 location.file_name(),
                 location.line(),
@@ -74,9 +74,9 @@ namespace utopia::core {
      * @param reason 不可到达的原因。
      * @param location 由编译器自动填写。调用位置。
     */
-    [[noreturn]] inline constexpr void unreachable(
-        const std::string_view reason,
-        std::source_location   location = std::source_location::current()) {
+    [[noreturn]] inline constexpr void
+        unreachable(const std::string_view reason,
+                    source_location location = source_location::current()) {
         u_assert(false, reason, location);
     }
 
---
+++ utopia-core/include/utopia/source_location.hpp
@@ -0,0 +1,50 @@
+// copy from <source_location> in Windows
+
+// with little modify
+// Copyright (c) Microsoft Corporation.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#include <cstdint>
+
+namespace utopia::core {
+    struct source_location {
+        // ------------------- because clang not supported
+        // ------------------↓ so change this from consteval to constexpr
+        [[nodiscard]] static constexpr source_location current(
+            const uint_least32_t _Line_     = __builtin_LINE(),
+            const uint_least32_t _Column_   = __builtin_COLUMN(),
+            const char *const    _File_     = __builtin_FILE(),
+            const char *const    _Function_ = __builtin_FUNCTION()) noexcept {
+            source_location _Result;
+            _Result._Line     = _Line_;
+            _Result._Column   = _Column_;
+            _Result._File     = _File_;
+            _Result._Function = _Function_;
+            return _Result;
+        }
+
+        constexpr source_location() noexcept = default;
+
+        [[nodiscard]] constexpr uint_least32_t line() const noexcept {
+            return _Line;
+        }
+        [[nodiscard]] constexpr uint_least32_t column() const noexcept {
+            return _Column;
+        }
+        [[nodiscard]] constexpr const char *file_name() const noexcept {
+            return _File;
+        }
+        [[nodiscard]] constexpr const char *function_name() const noexcept {
+            return _Function;
+        }
+
+      private:
+
+        uint_least32_t _Line{};
+        uint_least32_t _Column{};
+        const char    *_File     = "";
+        const char    *_Function = "";
+    };
+}   // namespace utopia::core
+
+#pragma once
